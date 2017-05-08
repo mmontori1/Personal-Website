a284ceda55152bc73587ff7f6aecc30538180b27
Mariano <mmontori@umich.edu>
2017-05-08 16:59:23 -0500

    practice/testing: states
    -learning about using states
    -messing around by using setTimeout and state

--- src/components/helog.jsx
+++ src/components/helog.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
+let change = 'switch'
+let message = 'hello to helog.com'
+alert(message)
+
 export default class Helog extends React.Component {
-  render() {
-    return (
-    	<h1> helog.com </h1>
-    );
-  }
+	constructor(){
+		super();
+		this.state = {
+			phrase: "helog",
+			tag: ".com"
+		}
+	}
+	deltaPhrase(){
+		if(this.state.phrase == "helog"){
+			console.log(change)
+			setTimeout(() => {
+				this.setState({phrase: "bomb"});
+			}, 2000)
+		}
+		else{
+			console.log(change)
+			setTimeout(() => {
+				this.setState({phrase: "helog"});
+			}, 2000)
+		}
+	}
+	render() {
+		// setTimeout(() => {
+		// 	this.setState({tag: ".com"});
+		// }, 3000)
+		this.deltaPhrase()
+		return (
+			<h1 style={{"textAlign" : "center"}} > {this.state.phrase + this.state.tag} </h1>
+		);
+	}
 }
