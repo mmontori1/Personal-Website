3ccc4af41e4fec14112188907624c9a8b0cb9378
Mariano <mmontori@umich.edu>
2017-05-20 11:26:09 -0500

    implemented state without redux
    -testing state without using mapStateToProps
    -app doesn’t work

--- src/app.jsx
+++ src/app.jsx
@@ -3,14 +3,12 @@ import { render } from 'react-dom';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter, routerMiddleware } from 'connected-react-router';
-// import createHistory from 'history/createBrowserHistory';
 import{
 	Home
 } from './pages';
 import { store, history } from './store.js';
 const app = document.getElementById('app');
-// const history = createHistory()
-// console.log(store)
+
 render(
 	<Provider store = {store}>
 		<ConnectedRouter history = {history}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,31 @@ import { connect } from 'react-redux'
 import { push } from 'connected-react-router'
 // import { 
 // } from './';
-// import { store } from '../store.js'
 
 const style = {
 	textAlign: "center"
 }
 
 class Home extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			phrase: "cats"
+		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleChange(e){
+		const phrase = e.target.value;
+		this.setState({phrase});
+	}
+
 	render() {
+		console.log(this.state.phrase)
 		return (
 			<div style = {style}>
 				<div> Page is a WIP </div>
-				<button onClick={this.props.goToCats}> go to cats </button>
+				<button onClick={this.props.goTo}> go to input phrase </button>
+				<input value = {this.state.phrase} onChange = {this.handleChange}/>
 			</div>
 		);
 	}
@@ -28,8 +41,9 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		goToCats: () => {
-			dispatch(push('/cats'))
+		goTo: () => {
+			var location = '/' + this.state.phrase
+			dispatch(push(location))
 		}
 	}
 }
