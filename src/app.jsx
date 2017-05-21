d86d9736bc13e82e02f59c9846a59c08204104c4
Mariano <mmontori@umich.edu>
2017-05-20 22:30:27 -0500

    restarting
    -trying redux over again
    -moving old code to other folder

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter, routerMiddleware } from 'connected-react-router';
-import{
-	Home
-} from './pages';
-import { store, history } from './store.js';
+// import { ConnectedRouter, routerMiddleware } from 'connected-react-router';
+// import{
+// 	Home
+// } from './pages';
+// import { store, history } from './store.js';
 const app = document.getElementById('app');
 
 render(
-	<Provider store = {store}>
-		<ConnectedRouter history = {history}>
-			<div>
-				<Route path = '/' component = {Home}/>
-				<Route path = '/cats' component = {Home}/>
-			</div>
-		</ConnectedRouter>	
-	</Provider>
+	<div>
+		Page is a WIP
+	</div>
 	, app);
