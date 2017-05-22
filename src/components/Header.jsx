213d2b304f29b391408c123ee688d2a9efa8a9f3
Mariano <mmontori@umich.edu>
2017-05-22 16:05:38 -0500

    navigator header css
    -not done the best way yet
    -will re-implement it with either:
          -css stylesheets through classes/id
          -styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-const blockStyle = {
-	textAlign: 'right'
-}
-
-const linkStyle = {
-	margin: '10px',
-}
-
 export default class Header extends React.Component {
 	render(){
 		return (
-			<div>
-				<div style = {blockStyle}>
-					<span style = {linkStyle}><Link to="/">Home</Link></span>
-					<span style = {linkStyle}><Link to="/about">About</Link></span>
-					<span style = {linkStyle}><Link to="/projects">Projects</Link></span>
-					<span style = {linkStyle}><Link to="/resume">Resume</Link></span>
-				</div>
-				<hr/>
-			</div>
+				<ul>
+					<li><Link to="/"><nav>Home</nav></Link></li>
+					<li><Link to="/about"><nav>About</nav></Link></li>
+					<li><Link to="/projects"><nav>Projects</nav></Link></li>
+					<li><Link to="/resume"><nav>Resume</nav></Link></li>
+				</ul>
 		)
 	}
 }
