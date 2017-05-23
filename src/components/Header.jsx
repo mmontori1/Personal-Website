3a757f51fc2113f0dce0dc8f7a8ecee43b2f9aea
Mariano <mmontori@umich.edu>
2017-05-23 13:09:02 -0500

    Header Styled Components
    -trying out styled components
    -redid header styling with styled components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+
+const NavBar = styled.ul`
+	list-style-type: none;
+    background-color: #222;
+    text-align: right;
+    margin: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+	right: 0;
+`;
+
+const Button = styled.li`
+	display: inline;
+`;
+
+const Word = styled.nav `
+	display: inline;
+	font-family: sans-serif;
+    display: inline-block;
+    color: aliceblue;
+    text-align: center;
+    padding: 12px 16px;
+    &:hover {
+	    background-color: #444;
+    	text-decoration: underline;
+	}
+`;
 
 export default class Header extends React.Component {
 	render(){
 		return (
-				<ul>
-					<li><Link to="/"><nav>Home</nav></Link></li>
-					<li><Link to="/about"><nav>About</nav></Link></li>
-					<li><Link to="/projects"><nav>Projects</nav></Link></li>
-					<li><Link to="/resume"><nav>Resume</nav></Link></li>
-				</ul>
+				<NavBar>
+					<Button><Link to="/"><Word>Home</Word></Link></Button>
+					<Button><Link to="/about"><Word>About</Word></Link></Button>
+					<Button><Link to="/projects"><Word>Projects</Word></Link></Button>
+					<Button><Link to="/resume"><Word>Resume</Word></Link></Button>
+				</NavBar>
 		)
 	}
 }
