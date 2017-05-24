8723b97ab0a4e3d110a60040d69f882920057a09
Mariano <mmontori@umich.edu>
2017-05-24 17:22:50 -0500

    MenuButton Component
    -Made a MenuButton Component for my Home Page
    -Cleaned up Home component
    -added styling to MenuButton
    -hover inverts color and insets shadow

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  display: block;
+  border-radius: 15px 50px 12px 40px;
+  box-shadow: 0px 0px 10px;
+  text-align: center;
+  width: 25%;
+  background: #04040C;
+  font-size: 3vw;
+  font-family: sans-serif;
+  color: #FFC036;
+  text-decoration: none;
+  margin: 10px 10px 15px 10%;
+  padding: 10px 24px;
+  overflow: hidden;
+  &:hover {
+      color: #04040C;
+      background: #FFC036;
+      box-shadow: inset 0 0 15px #000000;
+  }
+`;
+
+export default class MenuButton extends React.Component {
+	render(){
+		return (
+      <StyledLink to={this.props.route}>
+        {this.props.children}
+      </StyledLink>
+		)
+	}
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Header from './Header.jsx'
+import MenuButton from './MenuButton.jsx'
 import WIP from './WIP.jsx'
 
 export {
 	Header,
+	MenuButton,
 	WIP
 }
